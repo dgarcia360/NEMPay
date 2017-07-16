d3aa70538ab4cdb5a42fad1f6b60e6386fde2665
MIZUNASHI, Rin <mizunashi.rin@gmail.com>
2017-07-16 22:31:33 +0900

    japanese blowser test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -37,7 +37,7 @@ export class MyApp {
             });
 
             //i18n configuration
-            this.translateService.setDefaultLang('en');
+            this.translateService.setDefaultLang('ja');
             if (platform.is('cordova')) {
                 this.globalization.getPreferredLanguage()
                     .then(language => {
