f7448bd70ccbc95a6bff2eabad84fb4df38162fb
David <david@atraura.com>
2017-10-18 20:02:37 +0200

    ADD expected flow signup with private key #27

--- src/pages/signup_privatekey/signup.ts
+++ src/pages/signup_privatekey/signup.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {App, LoadingController} from 'ionic-angular';
+import {App, LoadingController, AlertController, Keyboard} from 'ionic-angular';
 import {BarcodeScanner} from '@ionic-native/barcode-scanner';
 
 import {TranslateService} from '@ngx-translate/core';
@@ -17,7 +17,7 @@ import {LoginPage} from '../login/login';
 export class SignupPrivateKeyPage {
     newAccount: any;
 
-    constructor(public app: App, private nem: NemProvider, private wallet: WalletProvider, private loading: LoadingController, private alert: AlertProvider, public translate: TranslateService, private barcodeScanner: BarcodeScanner) {
+    constructor(public app: App, private nem: NemProvider, private wallet: WalletProvider, private loading: LoadingController, private alertCtrl: AlertController, private alert: AlertProvider, public translate: TranslateService, private barcodeScanner: BarcodeScanner, private keyboard: Keyboard) {
         //sensitive data
         this.newAccount = null;
 
@@ -44,20 +44,60 @@ export class SignupPrivateKeyPage {
     public scanWalletQR() {
         this.barcodeScanner.scan().then(barcode => {
 
-            var walletInfo = JSON.parse(barcode.text);
-            if (!this.newAccount.password) {
-                this.alert.showBarCodeScannerRequiresPassword();
-            }
-            else if (this.newAccount.password != this.newAccount.repeat_password) {
-                this.alert.showPasswordDoNotMatch();
-            }
-            else {
-                try {
-                    this.newAccount.private_key = this.nem.decryptPrivateKey(this.newAccount.password, walletInfo);
-                } catch (err) {
-                    this.alert.showInvalidPasswordAlert();
-                }
-            }
+            let walletInfo = JSON.parse(barcode.text);
+
+            this.translate.get(['IMPORT_ACCOUNT_QR_WARNING', 'PASSWORD', 'CANCEL', 'CONFIRM', 'PLEASE_WAIT'], {}).subscribe((res) => {
+
+                let alert = this.alertCtrl.create({
+                    title: res['PASSWORD'],
+                    subTitle: res['IMPORT_ACCOUNT_QR_WARNING'],
+                    inputs: [
+                        {
+                            name: 'password',
+                            placeholder: res['PASSWORD'],
+                            type: 'password'
+                        },
+                    ],
+                    buttons: [
+                        {
+                            text: res['CANCEL'],
+                            role: 'cancel'
+                        },
+                        {
+                            text: res['CONFIRM'],
+                            handler: data => {
+                                this.keyboard.close();
+                                let loader = this.loading.create({
+                                    content: res['PLEASE_WAIT']
+                                });
+
+                                loader.present().then(
+                                    _ => {
+                                        if (data.password < 8) {
+                                            this.alert.showWeakPassword();
+                                            loader.dismiss();
+                                        }
+                                        else {
+                                            try {
+                                                this.newAccount.private_key = this.nem.decryptPrivateKey(data.password, walletInfo);
+                                                loader.dismiss();
+                                            } catch (err) {
+                                                this.alert.showInvalidPasswordAlert();
+                                                loader.dismiss();
+                                            }
+                                        }
+                                    });
+                            }
+                        }
+                    ]
+                });
+                alert.onDidDismiss(() => {
+                    this.keyboard.close();
+                });
+
+                alert.present();
+            });
+
         }).catch(err => {
             console.log("Error on scan");
         });
@@ -74,7 +114,7 @@ export class SignupPrivateKeyPage {
             this.alert.showInvalidPrivateKey();
         }
         else if (this.newAccount.password.length < 8) {
-            this.alert.showWeakPassword()
+            this.alert.showWeakPassword();
         }
         else {
 
@@ -84,7 +124,7 @@ export class SignupPrivateKeyPage {
                 });
 
                 loader.present().then(_ => {
-                    var createdWallet;
+                    let createdWallet;
                     try {
                         createdWallet = this.nem.createPrivateKeyWallet(this.newAccount.name, this.newAccount.password, this.newAccount.private_key);
                     }
@@ -101,9 +141,9 @@ export class SignupPrivateKeyPage {
                             }
                             else {
                                 this.wallet.storeWallet(createdWallet).then(value => {
-                                        loader.dismiss();
-                                        this._clearNewAccount();
-                                        this.app.getRootNav().push(LoginPage);
+                                    loader.dismiss();
+                                    this._clearNewAccount();
+                                    this.app.getRootNav().push(LoginPage);
                                 });
                             }
                         });
